332ab7acec5a365b7588345de2a383e895a9eb94
yappy <superunyo@yahoo.co.jp>
2018-10-19 20:50:15 +0900

    Add git describe info and write version info to a file

--- src/build.py
+++ src/build.py
@@ -5,6 +5,7 @@ import json
 from collections import OrderedDict
 
 def git_info() -> OrderedDict:
+	"""git コマンドでバージョン情報を取得する"""
 	# HEAD のコミット情報を取得
 	format = '--pretty=format:%H%n%s%n%ai%n%aI%n%at'
 	result = subprocess.run(['git', 'show', '--no-patch', format],
@@ -12,7 +13,16 @@ def git_info() -> OrderedDict:
 	outline = result.stdout.decode(sys.stdout.encoding)
 	hash, subject, date, date_iso8601, date_unix, *rest = outline.split('\n')
 
+	# 直近のタグ-タグからのコミット数-'g'hash-dirty?
+	# git describe --dirty --tags --always
+	result = subprocess.run(
+		['git', 'describe', '--dirty', '--tags', '--always'],
+		check=True, stdout=subprocess.PIPE)
+	outline = result.stdout.decode(sys.stdout.encoding)
+	describe, *rest = outline.split('\n')
+
 	head = OrderedDict()
+	head['describe'] = describe
 	head['hash'] = hash
 	head['subject'] = subject
 	head['date'] = date
@@ -32,12 +42,15 @@ def git_info() -> OrderedDict:
 	data['date_iso8601'] = date_iso8601
 	data['date_unix'] = date_unix
 
-	return { 'version': { 'repository': head, 'data': data } }
+	result = OrderedDict()
+	result['repository'] = head
+	result['data'] = data
+	return OrderedDict([('version', result)])
 
-# 1つの JSON ファイルにまとめる
-def merge_all(files: list, outfile: str):
+def merge_all(version: OrderedDict, files: list, outfile: str, verfile: str):
+	"""1つの JSON ファイルにまとめる"""
 	merged = OrderedDict()
-	merged.update(git_info())
+	merged.update(version)
 	for file_name in files:
 		with open(file_name, 'r') as f:
 			print('Processing...', file_name)
@@ -50,15 +63,19 @@ def merge_all(files: list, outfile: str):
 				# 無い場合は新たに追加する
 				else:
 					merged[k] = part[k]
-	print('Version info')
+	# git バージョン情報の表示と書き出し
+	print('Version info:', verfile)
 	print(json.dumps(merged['version']))
+	with open(verfile, 'w') as f:
+		json.dump(version, f, indent='\t', ensure_ascii=False)
+	# コンパクトファイルの書き出し
 	print('Writing...', outfile)
 	with open(outfile, 'w') as f:
 		json.dump(merged, f, separators=(',', ':'), ensure_ascii=False)
 	print('complete!')
 
-# 圧縮する
 def compress(file_in: str, file_out: str):
+	"""圧縮する"""
 	# 最高圧縮率で zip する
 	# 出力はリダイレクトせずそのまま流す
 	print('Run zip...')
@@ -82,9 +99,11 @@ def main(argv: list):
 
 	# 出力ファイル
 	merged_file = 'mtgdb.json'
+	version_file = 'version.json'
 	compressed_file = 'mtgdb.zip'
 
-	merge_all(srcs, merged_file)
+	version = git_info()
+	merge_all(version, srcs, merged_file, version_file)
 	compress(merged_file, compressed_file)
 
 # entry point
